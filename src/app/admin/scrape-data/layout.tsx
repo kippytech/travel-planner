9b33f8ae2f289b1b4d792604f6251ea961386129
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-02-22 21:08:34 +0300

    scraping frontend & sidebar

---
+++ src/app/admin/scrape-data/layout.tsx
@@ -0,0 +1,19 @@
+import Sidebar from "@/components/admin/sidebar/Sidebar";
+import React from "react";
+
+function AdminLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <section className="bg-[#f5f5fe] flex">
+      <Sidebar />
+      <section className="flex-1 flex-col flex">
+        <div className="h-48 bg-[#0E1428] text-white flex justify-center flex-col px-10 gap-3">
+          <h1 className="text-5xl">Scrape Data</h1>
+          <p>The scraping is powered by Daggy.</p>
+        </div>
+        {children}
+      </section>
+    </section>
+  );
+}
+
+export default AdminLayout;
--- src/components/admin/sidebar/Sidebar.tsx
+++ src/components/admin/sidebar/Sidebar.tsx
@@ -1,7 +1,126 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { BiSolidCategory } from "react-icons/bi";
+import { FaBookOpen, FaHome, FaHotel } from "react-icons/fa";
+import { LuLogOut } from "react-icons/lu";
+import { MdOutlineDataUsage } from "react-icons/md";
+import { Architects_Daughter } from "next/font/google";
+import {
+  Sidebar as ReactProSidebar,
+  Menu,
+  MenuItem,
+  sidebarClasses,
+} from "react-pro-sidebar";
+
+const architects_Daughter = Architects_Daughter({
+  weight: "400",
+  style: "normal",
+  subsets: ["latin"],
+});
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const router = useRouter();
+  const pathname = usePathname();
+  // State to keep track of the currently selected item
+  const [selectedItem, setSelectedItem] = useState("/admin/dashboard");
+  useEffect(() => {
+    setSelectedItem(pathname);
+  }, [pathname]);
+  const menuItems = [
+    { label: "Dashboard", icon: <FaHome />, link: "/admin/dashboard" },
+    {
+      label: "Trips",
+      icon: <BiSolidCategory />,
+      link: "/admin/trips",
+    },
+    {
+      label: "Hotels",
+      icon: <FaHotel />,
+      link: "/admin/hotels",
+    },
+    { label: "Bookings", icon: <FaBookOpen />, link: "/admin/bookings" },
+    {
+      label: "Scrape Data",
+      icon: <MdOutlineDataUsage />,
+      link: "/admin/scrape-data",
+    },
+  ];
+
+  const handleItemClick = (link: string) => {
+    // Update the selected item when a menu item is clicked
+    setSelectedItem(link);
+    router.push(link);
+  };
+
+  return (
+    <div className="min-h-[100vh]  overflow-hidden">
+      <ReactProSidebar
+        className="h-full overflow-hidden"
+        rootStyles={{
+          [`.${sidebarClasses.container}`]: {
+            backgroundColor: "#ffffff",
+            "&:hover": {
+              backgroundColor: "#ffffff",
+            },
+          },
+        }}
+      >
+        <Menu
+          className="h-[100vh] max-h-[100vh] text-black overflow-hidden"
+          menuItemStyles={{
+            button: ({ level, active }) => {
+              const backgroundColor = level === 0 ? "#ffffff" : "#ffffff";
+
+              return {
+                backgroundColor: active ? "#0E1428" : backgroundColor,
+                color: active ? "#ffffff" : "#000000",
+                "&:hover": {
+                  backgroundColor: active ? "#0E1428" : "#0E1428",
+                  color: active ? "#ffffff" : "#ffffff",
+                },
+              };
+            },
+          }}
+        >
+          <div className="flex items-center justify-center my-10 flex-col">
+            <Image
+              src="/logo.png"
+              alt="logo"
+              height={150}
+              width={150}
+              className="cursor-pointer"
+              onClick={() => router.push("/admin/dashboard")}
+            />
+            <span className="text-3xl uppercase font-medium italic">
+              <span className={architects_Daughter.className}>easy-book</span>
+            </span>
+          </div>
+
+          {menuItems.map((item, index) => (
+            <React.Fragment key={index}>
+              <MenuItem
+                onClick={() => handleItemClick(item.link)}
+                icon={item.icon}
+                active={selectedItem === item.link}
+              >
+                {item.label}
+              </MenuItem>
+            </React.Fragment>
+          ))}
+          <MenuItem
+            onClick={() => handleItemClick("/admin/logout")}
+            icon={<LuLogOut />}
+            active={selectedItem === "/admin/logout"}
+          >
+            Logout
+          </MenuItem>
+        </Menu>
+      </ReactProSidebar>
+    </div>
+  );
 }
 
 export default Sidebar;
