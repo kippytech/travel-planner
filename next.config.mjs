888f90e896a6d5be4c4c358298fc9e43c44a0693
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-02-24 13:30:06 +0300

    client trip search page & api route

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_DOMAIN: "http://localhost:3000",
   },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "imgcld.yatra.com",
+      },
+      {
+        hostname: "content.r9cdn.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,2 @@
 export { USER_API_ROUTES } from "./api-routes";
-//export { removeHtmlTags } from "./remove-html-tags";
+export { removeHtmlTags } from "./remove-html-tags";
